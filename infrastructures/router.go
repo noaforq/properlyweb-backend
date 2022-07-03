8733e9f09e1ecfe6b20b92c2d52eed7c763ef06c
noaforq <asano@properly.jp>
2022-07-03 19:09:48 +0900

    ハンズオンの初期状態

--- infrastructures/router.go
+++ infrastructures/router.go
@@ -14,7 +14,6 @@ func Run() {
 	e.Use(middleware.CORS())
 
 	routes.InitArticle(e)
-	routes.InitPerson(e)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- main.go
+++ main.go
@@ -1,25 +1,8 @@
 package main
 
 import (
-	"strconv"
-
 	"properly.jp/properlyweb-backend/infrastructures"
 )
-
-func FizzBuzz(num int) string {
-	if num < 1 || 100 < num {
-		panic("argument exception")
-	}
-	switch {
-	case num%15 == 0:
-		return "FizzBuzz"
-	case num%5 == 0:
-		return "Buzz"
-	case num%3 == 0:
-		return "Fizz"
-	}
-	return strconv.Itoa(num)
-}
 
 func main() {
 	infrastructures.Run()
